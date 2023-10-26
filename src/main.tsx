11de2ab70c976fe94e7417a2cfa21ab27dc9de78
st-lucphan <luc.phan@supremetech.vn>
2023-10-26 09:21:26 +0700

    Setup i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { I18nextProvider } from 'react-i18next';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import i18n from './app/core/services/i18n.service';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>,
 );
