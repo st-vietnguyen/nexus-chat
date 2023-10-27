a05b4146aa08676eabc5001be490badab27e7b3b
st-lucphan <luc.phan@supremetech.vn>
2023-10-27 16:50:55 +0700

    Remove unused files

--- src/app/core/auth/modules/custom-router-dom/router.interface.ts
+++
@@ -1,9 +0,0 @@
-import { LazyExoticComponent } from "react";
-
-export interface PageRoute {
-  path: string;
-  element?: LazyExoticComponent<() => JSX.Element>;
-  isProtected?: boolean; // default is false,
-  redirect?: string;
-  children?: PageRoute[];
-}
