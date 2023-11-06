16f8243ee82d52390823efdf047d79413fd23dbb
Huy Nguyen K <huy.nguyen@supremetech.vn>
2023-11-06 17:04:16 +0700

    Implement svg icon as React component

--- src/app/pages/home/containers/Home.tsx
+++ src/app/pages/home/containers/Home.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import Logo from '../../../../assets/react.svg?react';
 
 const Home = (): JSX.Element => {
   const { t } = useTranslation('common');
   return (
-    <div>{t('pages.homepage')}({process.env.APP_ENV})</div>
+    <>
+      <div>{t('pages.homepage')}({process.env.APP_ENV})</div>
+      <div>
+        <Logo />
+      </div>
+    </>
   );
 };
 
