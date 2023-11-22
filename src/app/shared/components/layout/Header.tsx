aea17c82cad00b5603ecfef05a3a7a49d8df02bc
st-lucphan <luc.phan@supremetech.vn>
2023-11-22 13:44:24 +0700

    Remove redux and create auth context

--- src/app/shared/components/layout/Header.tsx
+++ src/app/shared/components/layout/Header.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import i18next from 'i18next';
 
+import reactLogo from '/react.svg';
 import { LANGUAGES } from '@app/core/services/i18n.service';
+import { AuthContext } from '@app/shared/contexts/authContext';
+import { AuthService } from '@app/core/services/auth.service';
 
 export const Header = (): JSX.Element => {
+  const auth = new AuthService();
+  const { data, isAuthenticated, logout } = useContext(AuthContext);
 
   const changeLang = (lang: string) => {
     i18next.changeLanguage(lang);
+  };
+
+  const onLogout = async() => {
+    logout();
+    auth.removeToken();
   };
 
   return (
@@ -20,15 +30,24 @@ export const Header = (): JSX.Element => {
           <li className="menu-item">
             <Link to="articles">Articles</Link>
           </li>
-          <li className="menu-item">
-            <Link to="auth/login">Login</Link>
-          </li>
-          <li className="menu-item">
-            <Link to="auth/register">Register</Link>
-          </li>
+          {
+            isAuthenticated ?
+              <li className="menu-item">
+                <Link to="" onClick={onLogout}>Logout</Link>
+              </li>
+            :
+              <>
+                <li className="menu-item">
+                  <Link to="auth/login">Login</Link>
+                </li>
+                <li className="menu-item">
+                  <Link to="auth/register">Register</Link>
+                </li>
+              </>
+          }
         </ul>
       </nav>
-      <ul className="menu menu-lang d-flex">
+      <ul className="menu menu-lang">
         {LANGUAGES.map((lang: string) => {
           return (
             <li className="menu-item" key={lang}>
@@ -38,6 +57,9 @@ export const Header = (): JSX.Element => {
             </li>
           );
         })}
+        <li className="menu-item">
+          <img src={data?.image ? data.image : reactLogo} className="avatar" alt="Avatar" />
+        </li>
       </ul>
     </header>
   );
