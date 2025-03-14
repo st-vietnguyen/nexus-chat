9fd18d488789f3e91a9a99016eb880a79d2fafd6
st-canpham <can.pham@supremetech.vn>
2025-03-14 11:01:23 +0700

    Add pages-container class

--- src/app/AppErrorBoundaryFallback.tsx
+++ src/app/AppErrorBoundaryFallback.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AppErrorBoundaryFallback = ({ error, resetErrorBoundary }) => {
   return (
-    <div>
+    <div className="pages-container">
       <h2 className='txt-bold'>Something went wrong!</h2>
       <p>{error.message}</p>
       <button onClick={resetErrorBoundary}>Try again</button>
