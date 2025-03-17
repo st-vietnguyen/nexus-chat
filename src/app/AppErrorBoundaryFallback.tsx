231105488814b312fbcb41a8c900159438f68d88
st-canpham <can.pham@supremetech.vn>
2025-03-17 15:27:40 +0700

    Update UI Error Boundary

--- src/app/AppErrorBoundaryFallback.tsx
+++ src/app/AppErrorBoundaryFallback.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import AlertError from '@assets/alert-error.svg?react';
 
 const AppErrorBoundaryFallback = ({ error, resetErrorBoundary }) => {
   return (
-    <div className="pages-container">
-      <h2 className='txt-bold'>Something went wrong!</h2>
-      <p>{error.message}</p>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
+      <div className="pages-container">
+        <AlertError className="error-icon"/>
+        <h2 className="txt-bold">Opps, Something went wrong...</h2>
+        <p className="error-message">{error.message}</p>
+        <button onClick={resetErrorBoundary}>Try again</button>
+      </div>
   );
 };
 
