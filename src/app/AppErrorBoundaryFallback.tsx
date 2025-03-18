2dcd54bf527b0028aa680b75c2db0f5709ca7f63
st-canpham <can.pham@supremetech.vn>
2025-03-18 16:34:21 +0700

    Fix import svg icon

--- src/app/AppErrorBoundaryFallback.tsx
+++ src/app/AppErrorBoundaryFallback.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import AlertError from '@assets/icons/alert-error.svg?react';
+import alertError from '/icons/alert-error.svg';
 
 const AppErrorBoundaryFallback = ({ error, resetErrorBoundary }) => {
   return (
     <div className="pages-container">
-      <AlertError className="error-icon" />
+      <img className="error-icon" src={alertError} />
       <h2 className="txt-bold">Opps, Something went wrong...</h2>
       <p className="error-message">{error.message}</p>
       <button onClick={resetErrorBoundary} className="btn btn-primary">
