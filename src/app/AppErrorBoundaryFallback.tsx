6141aaa0d3c79253f2b028721b3eab2e97558037
st-canpham <can.pham@supremetech.vn>
2025-03-18 11:22:22 +0700

    Create a style for the button module based on the Bootstrap concept

--- src/app/AppErrorBoundaryFallback.tsx
+++ src/app/AppErrorBoundaryFallback.tsx
@@ -7,7 +7,7 @@ const AppErrorBoundaryFallback = ({ error, resetErrorBoundary }) => {
         <AlertError className="error-icon"/>
         <h2 className="txt-bold">Opps, Something went wrong...</h2>
         <p className="error-message">{error.message}</p>
-        <button onClick={resetErrorBoundary} className="button button-error">Try again</button>
+        <button onClick={resetErrorBoundary} className="btn btn-error">Try again</button>
       </div>
   );
 };
