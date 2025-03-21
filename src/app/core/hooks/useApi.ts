314ff81bc5e6f96f6c8d6cf75ff5beb51b9f80ae
st-longngo <long.ngo@supremetech.vn>
2025-03-21 10:42:29 +0700

    Move folder to core

--- src/app/core/hooks/useApi.ts
+++ src/app/core/hooks/useApi.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from 'react';
 import { AxiosError } from 'axios';
 
-import { ApiService } from '@core/services/api.service';
+import { ApiService } from '../services/api.service';
 
 const apiService = new ApiService();
 
