07d912502e25e9680f927fb8b6ca0dee04cfb9d9
st-canpham <can.pham@supremetech.vn>
2025-04-01 11:52:21 +0700

    Apply Redux Toolkit with a basic example

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { I18nextProvider } from 'react-i18next';
 import { createRoot } from 'react-dom/client';
 import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
+import { Provider } from 'react-redux';
 
 import i18n from './core/services/i18n.service';
 import '@stylesheet/styles.scss';
@@ -13,6 +14,7 @@ import AppSuspense from './AppSuspense';
 import { AuthProvider } from './shared/contexts/auth.context';
 import { renderChildren } from './core/modules/custom-router-dom/RouterOutlet';
 import AppErrorBoundaryFallback from './AppErrorBoundaryFallback';
+import { store } from './store';
 
 export const Root = () => {
   return (
@@ -32,9 +34,11 @@ const router = createBrowserRouter([
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <I18nextProvider i18n={i18n}>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </I18nextProvider>,
+  <Provider store={store}>
+    <I18nextProvider i18n={i18n}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </I18nextProvider>
+  </Provider>,
 );
