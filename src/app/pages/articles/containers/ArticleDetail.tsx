8ca5d976252b8f5898975ae406b07bbf491adafa
st-canpham <can.pham@supremetech.vn>
2025-04-01 11:59:53 +0700

    Handle fetch and show article detail

--- src/app/pages/articles/containers/ArticleDetail.tsx
+++ src/app/pages/articles/containers/ArticleDetail.tsx
@@ -1,7 +1,28 @@
+import { AppDispatch, RootState } from '@app/store';
+import { fetchArticleDetail } from '@app/store/slices/articleSlice';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { FetchingStatus } from '@app/shared/enum/fetchingStatus';
+import ArticleDetailContent from '../components/ArticleDetailContent';
 
 const ArticleDetail = () => {
-  return <div>This is article-detail page</div>;
+  const dispatch = useDispatch<AppDispatch>();
+  const { article, status } = useSelector((state: RootState) => state.article);
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    if (id) {
+      dispatch(fetchArticleDetail(id));
+    }
+  }, [id]);
+
+  return (
+    <ArticleDetailContent
+      isLoading={status === FetchingStatus.LOADING}
+      article={article}
+    />
+  );
 };
 
 export default ArticleDetail;
