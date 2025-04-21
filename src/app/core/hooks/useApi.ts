989129dcfca1e7a74669edffbdc12868ff148e13
st-longngo <long.ngo@supremetech.vn>
2025-04-21 15:37:49 +0700

    Remove code unuse

--- src/app/core/hooks/useApi.ts
+++ src/app/core/hooks/useApi.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState } from 'react';
 import { AxiosError } from 'axios';
 
 import { ApiService } from '../services/api.service';
