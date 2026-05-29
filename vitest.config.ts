35767a14d2349fb3579dd9d90aecf3d387c3d901
Viet Nguyen T. <viet.nguyen@supremetech.vn>
2026-05-29 22:21:02 +0700

    feat: add test

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitest/config';
+import Path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@app': Path.resolve(__dirname, './src/app'),
+      '@config': Path.resolve(__dirname, './src/config'),
+      '@stylesheet': Path.resolve(__dirname, './src/stylesheet'),
+      '@shared': Path.resolve(__dirname, './src/app/shared'),
+      '@core': Path.resolve(__dirname, './src/app/core'),
+      '@assets': Path.resolve(__dirname, './src/assets'),
+      '@libs': Path.resolve(__dirname, './src/app/libs'),
+    },
+  },
+  test: {
+    environment: 'happy-dom',
+    globals: true,
+    include: ['src/**/*.test.{ts,tsx}'],
+  },
+});
